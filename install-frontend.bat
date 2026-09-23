2bf95e51f2ff6efb81824d844f5ae0f2b0a882f6
Ruine <davidarandia0715@gmail.com>
2026-09-23 08:57:21 +0800

    Deploy complete cat storefront and remove legacy assets

--- install-frontend.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0frontend"
-set npm_config_yes=true
-set npm_config_audit=false
-set npm_config_fund=false
-"%ProgramFiles%\nodejs\npm.cmd" install --yes --no-audit --no-fund
-if errorlevel 1 exit /b %errorlevel%
-"%ProgramFiles%\nodejs\npm.cmd" run build
-exit /b %errorlevel%
